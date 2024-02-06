08f018106c84db594a3045ef113a12bfda6bcfd9
nainglinnkhant <nainglinnkhnt@gmail.com>
2024-02-06 22:05:41 +0630

    feat: add dynamic module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
       synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
     }),
     CoffeeRatingModule,
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/coffee-rating/coffee-rating.module.ts
+++ src/coffee-rating/coffee-rating.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CoffeeRatingService } from './coffee-rating.service';
 import { CoffeesModule } from 'src/coffees/coffees.module';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    DatabaseModule.register({
+      type: 'postgres',
+      host: 'localhost',
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+    }),
+  ],
   providers: [CoffeeRatingService],
 })
 export class CoffeeRatingModule {}
