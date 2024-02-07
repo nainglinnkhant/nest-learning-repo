b6549a11597909af0e6396cb6035cf03fe914c91
nainglinnkhant <nainglinnkhnt@gmail.com>
2024-02-08 02:41:12 +0630

    docs: group endpoints with api tags

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -14,7 +14,9 @@ import { UpdateCoffeeDto } from './dto/update-coffee-dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 import { Public } from 'src/common/decorators/public.decorator';
 import { ParseIntPipe } from 'src/common/pipes/parse-int.pipe';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('coffees')
 @Controller('coffees')
 export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
