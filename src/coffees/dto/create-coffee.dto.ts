dc1817e58f75299453053537c78e3dba9881b965
nainglinnkhant <nainglinnkhnt@gmail.com>
2024-02-08 02:30:41 +0630

    docs: add descriptions and examples to dtos in swagger docs

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,12 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class CreateCoffeeDto {
+  @ApiProperty({ description: 'The name of a coffee.' })
   @IsString()
   readonly name: string;
 
+  @ApiProperty({ description: 'The brand of a coffee.' })
   @IsString()
   readonly brand: string;
 
+  @ApiProperty({
+    description: 'The flavors of a coffee',
+    example: ['caramel', 'vanilla'],
+  })
   @IsString({ each: true })
   readonly flavors: string[];
 }
