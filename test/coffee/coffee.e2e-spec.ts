af278d99f77230670146dc06b35b0a589a358176
nainglinnkhant <nainglinnkhnt@gmail.com>
2024-02-09 02:03:14 +0630

    test: add e2e test for coffee module

---
+++ test/coffee/coffee.e2e-spec.ts
@@ -0,0 +1,124 @@
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as request from 'supertest';
+import { UpdateCoffeeDto } from 'src/coffees/dto/update-coffee-dto';
+import { HttpExceptionFilter } from 'src/common/filters/http-exception.filter';
+import { TimeoutInterceptor } from 'src/common/interceptors/timeout.interceptor';
+import { WrapResponseInterceptor } from 'src/common/interceptors/wrap-response.interceptor';
+import { CoffeesModule } from '../../src/coffees/coffees.module';
+
+describe('[Feature] Coffees - /coffees', () => {
+  let app: INestApplication;
+  let httpServer;
+  const coffee = {
+    name: 'Shipwreck Roast',
+    brand: 'Buddy Brew',
+    flavors: ['chocolate', 'vanilla'],
+  };
+  const expectedPartialCoffee = expect.objectContaining({
+    ...coffee,
+    flavors: expect.arrayContaining(
+      coffee.flavors.map((name) => expect.objectContaining({ name })),
+    ),
+  });
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [
+        CoffeesModule,
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: 'localhost',
+          port: 5433,
+          username: 'postgres',
+          password: 'pass123',
+          database: 'postgres',
+          autoLoadEntities: true,
+          synchronize: true,
+        }),
+      ],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        transform: true,
+        forbidNonWhitelisted: true,
+        transformOptions: {
+          // @Type(() => Number) is needed anymore in the PaginationQueryDTO
+          enableImplicitConversion: true,
+        },
+      }),
+    );
+    app.useGlobalFilters(new HttpExceptionFilter());
+    app.useGlobalInterceptors(
+      new WrapResponseInterceptor(),
+      new TimeoutInterceptor(),
+    );
+    await app.init();
+    httpServer = app.getHttpServer();
+  });
+
+  it('Create [POST /]', () => {
+    return request(httpServer)
+      .post('/coffees')
+      .send(coffee)
+      .expect(HttpStatus.CREATED)
+      .then(({ body }) => {
+        expect(body).toEqual(expectedPartialCoffee);
+      });
+  });
+
+  it('Get all [GET /]', () => {
+    return request(httpServer)
+      .get('/coffees')
+      .then(({ body }) => {
+        expect(body.length).toBeGreaterThan(0);
+        expect(body[0]).toEqual(expectedPartialCoffee);
+      });
+  });
+
+  it('Get one [GET /:id]', () => {
+    return request(httpServer)
+      .get('/coffees/1')
+      .then(({ body }) => {
+        expect(body).toEqual(expectedPartialCoffee);
+      });
+  });
+
+  it('Update one [PATCH /:id]', () => {
+    const updateCoffeeDto: UpdateCoffeeDto = {
+      ...coffee,
+      name: 'New and Improved Shipwreck Roast',
+    };
+    return request(httpServer)
+      .patch('/coffees/1')
+      .send(updateCoffeeDto)
+      .then(({ body }) => {
+        expect(body.name).toEqual(updateCoffeeDto.name);
+
+        return request(httpServer)
+          .get('/coffees/1')
+          .then(({ body }) => {
+            expect(body.name).toEqual(updateCoffeeDto.name);
+          });
+      });
+  });
+
+  it('Delete one [DELETE /:id]', () => {
+    return request(httpServer)
+      .delete('/coffees/1')
+      .expect(HttpStatus.OK)
+      .then(() => {
+        return request(httpServer)
+          .get('/coffees/1')
+          .expect(HttpStatus.NOT_FOUND);
+      });
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
